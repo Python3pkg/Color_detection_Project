5b0e88298f93c1a32b01c4f0f593d5e286c4854e
flaviencharton <flavien.charton@gmail.com>
2017-03-31 10:40:23 +0200

    Updated

---
+++ README.md
@@ -0,0 +1,81 @@
+# color_detection
+
+
+################################# DESCRIPTION #################################
+Author : Flavien CHARTON
+Color detection program for RGB images
+
+A short program to find the percentage of a range color given by the user
+in an image and write the resulting image with the minimum values of the given 
+range
+
+################################ INSTALLATION ##################################
+Dependencies : python 2.X, OpenCV
+
+In order to use this program, please install Python programming language 
+(vers. 2.X) and OpenCV.
+
+********* Installation of Python 2.X version (vers. 2.7.Y recommended) *********
+
+On Windows: 
+
+	- Download Python MSI Installer from the Python Downloads page
+	 http://www.python.org/downloads/ (x86 for 32 bit OS preferable)
+	- Double click on the MSi file
+	- Follow the instructions from the installer
+	- During installation, check in the command prompt if "pip" is going to 
+	 be installed
+	- If not, click on the link below :
+	 https://pip.pypa.io/en/stable/installing/
+	- After installation completed, check the PATH environment variable
+	- If not already set, add the following to your PATH (replace X):
+	 C:\Python2X\;C:\Python2X\Scripts\
+	 (Assuming Python 2.X is intalled in C: root) 
+
+On Linux:
+
+   The latest versions of CentOS, Fedora, Redhat Enterprise (RHEL) and Ubuntu come
+   with Python 2.7.
+
+	- Open a command prompt and do the following to check which version of
+	 Python is installed:
+	 python --version
+	- For Debian and Ubuntu Linux if Python is not installed, do
+	 the following (replace X):
+	 apt-cache search python | egrep "^python2.[0-9]" --color
+	 sudo apt-get install python2.X
+	- For Red Hat/ RHEL / CentOS Linux, do the follong:
+	 sudo yum install python
+	- Repeat the first step (check python version)
+	- Open a command prompt and do the following to check if "pip" 
+	 is installed:
+	 command -v pip
+	- To install "pip", follow the official installation guide:
+	 https://pip.pypa.io/en/stable/installing/
+
+**************************** Installation of OpenCV *******************************
+
+On Windows:
+
+	- Go to the following site and follow the instructions (OpenCV from prebuilt binaries):
+	 http://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_setup/py_setup_in_windows/py_setup_in_windows.html
+
+On Linux:
+
+   	- Go to the following site and follow the instructions:
+	 http://docs.opencv.org/2.4/doc/tutorials/introduction/linux_install/linux_install.html
+
+############################# RUNNING THE SCRIPT ###############################
+
+On Windows:
+
+   Open a command prompt and do the following (replace X):
+	C:\Python2X\python.exe C:\<PYTHON_FILE_PATH>\color_detection.py
+   Or do the following (only if Python is in your PATH environment variable):
+	python.exe C:\<PYTHON_FILE_PATH>\color_detection.py
+
+On Linux:
+
+   Open a command prompt and do the following:
+	chmod +x color_detection.py
+	python color_detection.py
---
+++ color_detection/color_detection.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 18 18:30:12 2016
+
+A short program to find the percentage of a range color given by the user
+in an image and write the resulting image with the minimum values of the range
+
+@author: Flavien CHARTON
+"""
+
+import os, argparse, glob, sys, datetime
+import numpy as np
+import cv2
+
+def color_detection(file):
+    global filename
+    filename = os.path.splitext(os.path.basename(file))[0]
+    colorCounter = 0
+    image = cv2.imread(file, 1)
+    
+    # Convert BGR to HSV
+    HSVimage = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    # define range of color in HSV
+    lowerValue = np.array([minHue, 0, 0], dtype = np.uint8)
+    upperValue = np.array([maxHue, 255, 255], dtype = np.uint8)
+
+    # Threshold the HSV image to get only green colors
+    mask = cv2.inRange(HSVimage, lowerValue, upperValue)
+    
+    for i in range(mask.shape[0]):
+        for j in range(mask.shape[1]):
+            if(mask[i, j] == 255):
+                colorCounter += 1
+                
+    colorPercent = (colorCounter * 100) / (float(mask.shape[0]) * float(mask.shape[1]))
+
+    # Bitwise-AND mask and original image
+    result = cv2.bitwise_and(image, image, mask = mask)
+
+    cv2.imwrite(args['result'] + '/color_detection_' + filename + '.png', result)
+    
+    print("\nPercentage of pixels in Hue range " + str((minHue, maxHue)) + " for " + filename + " image: " + str(colorPercent) + " %")
+    return colorCounter
+    
+if(__name__ == '__main__'):
+    global minHue, maxHue
+    print("\nColor detection program for RGB images (dependencies : python 2.X, PIL)")
+    
+    tries = 0
+    while(True):
+        dirs = raw_input("Enter a path to the images you want to process (eg. C:/some_image_directory/): ")
+        if(os.path.exists(dirs)):
+            break
+        else:
+            print("\nSorry, we failed to find an existing path.\n")
+            tries += 1
+        if(tries == 3):
+            sys.exit()
+    tries = 0
+    while(True):
+        minHue, maxHue = input("Enter a Hue range to find in images you want to process (eg : minHue, maxHue): ")
+        if(type(minHue), type(maxHue) == int, int):
+            print("\nPlease wait while processing...")            
+            break
+        else:
+            print("\nSorry, wrong variable type, please enter integer variables.\n")
+            tries += 1
+        if(tries == 3):
+            print("\nEnd of tries.\n")
+            sys.exit()
+    
+    resultPath = dirs + str(datetime.date.today()) + "_color_detection_results"
+    if not(os.path.exists(dirs + str(datetime.date.today()) + "_color_detection_results")):
+        os.makedirs(dirs + str(datetime.date.today()) + "_color_detection_results")
+        
+    #Build the argument parser and parse the arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path', required = True, help = "Path to image files")
+    parser.add_argument('--result', required = True, help = "Path to resulting files")
+    args = vars(parser.parse_args(['--path', dirs, '--result', resultPath]))
+    
+    for file in glob.glob(args['path'] + '*'):
+        #Check file is a real file and check its extension
+        if(os.path.isfile(file) and file.endswith(('.png', '.jpg', '.JPG', '.jpeg'))):
+            color_detection(file)
+        else:
+            continue
+        
+    print("\nSuccessfully processed.")
