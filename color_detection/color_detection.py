29b83e51304071a8127bcc10f1bd26fd6188ddc3
flaviencharton <flavien.charton@gmail.com>
2017-03-31 13:50:04 +0200

    Update color_detection.py

--- color_detection/color_detection.py
+++ color_detection/color_detection.py
@@ -3,7 +3,7 @@
 Created on Mon Jul 18 18:30:12 2016
 
 A short software that enables the user to find the percentage 
-of a range color given by the user in a natural image and 
+of a color range given by the user in a natural image and 
 save the resulting image with the minimum values of the 
 given range
 
